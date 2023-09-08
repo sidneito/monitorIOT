743e87ced2f637e5223141065d20aa5e4fc8edd0
sidneito <sidneito@gmail.com>
2023-09-08 00:27:55 -0300

    add rotas

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,19 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Rota para o painel de administração
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rota para a página de consulta de dados no painel de administração
+Route::get('/admin/consulta', 'AdminController@consulta')->name('admin.consulta');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Rota para a página de detalhes de máquinas no painel de administração
+Route::get('/admin/maquinas/{id}', 'AdminController@detalhesMaquina')->name('admin.detalhes_maquina');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Rota para a API de recebimento de dados das máquinas
+Route::post('/api/machines', 'MachineController@receberDados')->name('api.receber_dados');
+
 
 require __DIR__.'/auth.php';
