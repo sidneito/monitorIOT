fa7f6d6903fdea2707613d8f5e00ef1be78502f4
sidneito <sidneito@gmail.com>
2023-09-08 00:54:00 -0300

    ultima de hj

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
 Route::view('/home', 'welcome');
+Route::get('/admin', 'AdminController@index');
+
 
 require __DIR__.'/auth.php';
